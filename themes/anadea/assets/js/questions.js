0e43aa30fea2eed1dc193850783ec55b4456083e
his <his@anadeainc.com>
2024-03-06 17:05:03 +0200

    add questions partial

---
+++ themes/anadea/assets/js/questions.js
@@ -0,0 +1,6 @@
+function questionToggler () {
+  const controls = document.querySelectorAll('.questions__control')
+  console.log(controls)
+}
+
+document.addEventListener('DOMContentLoaded', questionToggler)
