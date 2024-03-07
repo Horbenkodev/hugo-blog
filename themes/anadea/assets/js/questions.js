33aa754f8f1308dfcceafa52fa54cfd5cfbb9924
his <his@anadeainc.com>
2024-03-07 16:06:11 +0200

    cleanup

--- themes/anadea/assets/js/questions.js
+++
@@ -1,6 +0,0 @@
-function questionToggler () {
-  const controls = document.querySelectorAll('.questions__control')
-  console.log(controls)
-}
-
-document.addEventListener('DOMContentLoaded', questionToggler)
