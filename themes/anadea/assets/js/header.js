c572d85b6ab544b602a8fc4004a97146cf2cc536
his <his@anadeainc.com>
2024-03-28 17:43:33 +0200

    improve UX

--- themes/anadea/assets/js/header.js
+++ themes/anadea/assets/js/header.js
@@ -2,24 +2,37 @@ function burgerToggler () {
   const controls = document.querySelectorAll('.header__burger')
   let isActive = false
 
+  const inner = document.querySelector('.header__inner')
+  const menu = document.querySelector('.header__menu')
+  const shadow = document.querySelector('.header__shadow')
+
+  function activate () {
+    inner.classList.add('active')
+    menu.classList.add('active')
+    shadow.classList.add('active')
+    document.body.style.overflow = 'hidden'
+  }
+
+  function deactivate () {
+    document.body.style.overflow = ''
+    inner.classList.remove('active')
+    menu.classList.remove('active')
+    shadow.classList.remove('active')
+  }
+
   controls.forEach((element) => {
     element.addEventListener('click', () => {
-      const inner = document.querySelector('.header__inner')
-      const menu = document.querySelector('.header__menu')
-      const shadow = document.querySelector('.header__shadow')
-      inner.classList.toggle('active')
-      menu.classList.toggle('active')
-      shadow.classList.toggle('active')
-
       isActive = !isActive
 
       if (isActive) {
-        document.body.style.overflow = 'hidden'
+        activate()
       } else {
-        document.body.style.overflow = ''
+        deactivate()
       }
     })
   })
+
+  window.addEventListener('resize', deactivate)
 }
 
 document.addEventListener('DOMContentLoaded', burgerToggler)
