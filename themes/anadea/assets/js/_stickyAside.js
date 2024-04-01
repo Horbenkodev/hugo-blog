2d37d2373ebbdc6d3fdf628c0953ba01104f808f
Eduard Kolenov <erk@anadeainc.com>
2024-04-01 14:47:16 +0300

    refactor animate aside on page scroll

--- themes/anadea/assets/js/_stickyAside.js
+++ themes/anadea/assets/js/_stickyAside.js
@@ -1,13 +1,10 @@
 function observe() {
   const authorConnect = document.querySelector('#author-connect');
-  const tocDesktopNode = document.querySelector('#toc');
+  const tocDesktop = document.querySelector('#toc');
+  const readNext = document.querySelector('#read-next');
 
-  function setAuthorConnectHeight() {
-    const authorConnectHeight = authorConnect.getBoundingClientRect().height;
-    authorConnect.style = `top: calc(100vh - ${authorConnectHeight}px)`;
-  }
-
-  window.addEventListener('resize', setAuthorConnectHeight);
+  const readNextHeight = readNext.getBoundingClientRect().height + 60;
+  readNext.style = `top: calc(100vh - ${readNextHeight}px)`;
 
   // ToC observer
   function tocCallback(entries) {
@@ -21,23 +18,47 @@ function observe() {
   }
 
   const tocObserver = new IntersectionObserver(tocCallback);
-  tocObserver.observe(tocDesktopNode);
+  tocObserver.observe(tocDesktop);
 
-  // Read Next node observer
-  const readNextNode = document.querySelector('#read-next');
+  // Post node observer
+  const postNodeSentinel = document.querySelector('#sentinel');
+  const postNode = document.querySelector('.markdown');
+  const postNodeRect = postNode.getBoundingClientRect();
+  const bodyRect = document.body.getBoundingClientRect();
+  const postTopOffset = postNodeRect.top - bodyRect.top;
+  const marginTop = postTopOffset + postNodeRect.height * 0.8 - window.screen.height;
 
-  function readNextCallback(entries) {
+  const postObserverOptions = {
+    root: null,
+    rootMargin: `${marginTop}px`,
+  };
+
+  function postCallback(entries) {
     if (entries[0].isIntersecting) {
-      authorConnect.classList.remove('sticky');
-      authorConnect.style = `margin-top: auto;`;
+      readNext.classList.remove('visible');
+      readNext.classList.add('hidden');
+      authorConnect.style.display = 'block';
+
+      setTimeout(() => {
+        authorConnect.classList.add('sticky');
+        authorConnect.classList.remove('hidden');
+        authorConnect.classList.add('visible');
+      }, 300);
     } else {
-      setAuthorConnectHeight();
-      authorConnect.classList.add('sticky');
+      authorConnect.classList.remove('sticky');
+      authorConnect.classList.remove('visible');
+      authorConnect.classList.add('hidden');
+
+      setTimeout(() => {
+        authorConnect.style.display = 'none';
+        readNext.classList.remove('hidden');
+        readNext.classList.add('visible');
+      }, 300);
     }
   }
 
-  const readNextNodeObserver = new IntersectionObserver(readNextCallback);
-  readNextNodeObserver.observe(readNextNode);
+  const postObserver = new IntersectionObserver(postCallback, postObserverOptions);
+  postObserver.observe(postNodeSentinel);
 }
 
 document.addEventListener('DOMContentLoaded', observe, { once: true });
