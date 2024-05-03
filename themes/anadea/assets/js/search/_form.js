7f85d4b33ef4fadcc0f020066e336afd5bdce2be
his <his@anadeainc.com>
2024-05-03 20:33:03 +0300

    cleanup search query

--- themes/anadea/assets/js/search/_form.js
+++ themes/anadea/assets/js/search/_form.js
@@ -6,7 +6,14 @@ function initForm() {
   search.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    const data = new URLSearchParams(new FormData(search));
+    const formData = new FormData(search);
+    const filteredFormData = new FormData();
+
+    for (const [key, value] of formData) {
+      if (value) filteredFormData.set(key, value);
+    }
+
+    const data = new URLSearchParams(filteredFormData);
     const token = data.get('token');
     const categories = data.get('categories');
     const industries = data.get('industries');
