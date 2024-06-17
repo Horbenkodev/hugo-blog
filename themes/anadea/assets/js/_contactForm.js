f172aecb4f52327a2dd4a4b892df4d7fca73fecd
his <his@anadeainc.com>
2024-06-17 22:28:51 +0300

    refactoring

--- themes/anadea/assets/js/_contactForm.js
+++ themes/anadea/assets/js/_contactForm.js
@@ -1,50 +1,32 @@
 import { parseCookie } from './utils/parseCookie';
 import { truncateText } from './utils/truncateText';
 
-function processContactForm() {
-  const contactForm = document.getElementById('contact_form');
+function createField(name, value, contactForm) {
+  var field = document.createElement('input');
+  field.setAttribute('type', 'hidden');
+  field.setAttribute('name', name);
+  field.setAttribute('value', value);
+  contactForm.appendChild(field);
+}
 
-  // add guid field
-  var guid = parseCookie(document.cookie)._ga;
-  var guidField = document.createElement('input');
-  guidField.setAttribute('type', 'hidden');
-  guidField.setAttribute('name', 'guid');
-  guidField.setAttribute('value', guid);
-  contactForm.appendChild(guidField);
+function addGuidField(contactForm) {
+  const guid = parseCookie(document.cookie)._ga;
+  createField('guid', guid, contactForm);
+}
 
-  // change attachment file name
+function addFileNameChangeListener(contactForm) {
   const fileField = contactForm.querySelector('#file');
   const label = contactForm.querySelector('.contacts__label');
   const fileLabel = contactForm.querySelector('#file-label');
 
-  fileField.addEventListener('change', function (event) {
-    if (event.target.files.length > 0) {
-      label.textContent = `${event.target.files[0].name}`;
-    } else {
-      label.textContent = 'Attach file';
-    }
-
-    const fileName = event.target.files[0].name;
+  fileField.addEventListener('change', (event) => {
+    const fileName = event.target.files.length > 0 ? event.target.files[0].name : 'Attach file';
+    label.textContent = fileName;
     fileLabel.textContent = truncateText(fileName, 20);
   });
+}
 
-  // add remote IP field
-  fetch('https://api.ipify.org?format=json')
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (json) {
-      var remoteIPField = document.createElement('input');
-      remoteIPField.setAttribute('type', 'hidden');
-      remoteIPField.setAttribute('name', 'remote_ip');
-      remoteIPField.setAttribute('value', json.ip);
-      document.getElementById('contact_form').appendChild(remoteIPField);
-    })
-    .catch(function (err) {
-      console.error('Error getting IP Address: '.concat(err));
-    });
-
-  // init GTM listener
+function initGtmListener(contactForm) {
   if (window.dataLayer) {
     var oldPush = window.dataLayer.push;
     window.dataLayer.push = function () {
@@ -56,48 +38,59 @@ function processContactForm() {
       ) {
         var client_id = arguments[0]['h']['gtagApiResult']['client_id'];
         if (document.getElementsByName('gclid').length == 0) {
-          var gclidField = document.createElement('input');
-          gclidField.setAttribute('type', 'hidden');
-          gclidField.setAttribute('name', 'gclid');
-          gclidField.setAttribute('value', client_id);
-          contactForm.appendChild(gclidField);
+          createField('gclid', client_id, contactForm);
         }
       }
     };
   }
+}
 
-  // add referrer url field
-  var referrerField = document.createElement('input');
-  referrerField.setAttribute('type', 'hidden');
-  referrerField.setAttribute('name', 'referer_url');
-  referrerField.setAttribute('value', document.referrer);
-  contactForm.appendChild(referrerField);
+function addReferrerUrlField(contactForm) {
+  createField('referer_url', document.referrer, contactForm);
+}
 
-  if (!('first-url' in localStorage)) {
+function addFirstUrlField(contactForm) {
+  if (!localStorage.getItem('first-url')) {
     localStorage.setItem('first-url', window.location.href);
   }
 
-  // submit form
-  function handleSubmit(e) {
-    e.preventDefault();
-    const formData = new FormData(this);
+  const localStorageItem = localStorage.getItem('first-url');
+  createField('first_url', localStorageItem, contactForm);
+  localStorage.removeItem('first-url');
+}
 
-    if (document.getElementsByName('first_url').length == 0) {
-      var firstUrlField = document.createElement('input');
-      firstUrlField.setAttribute('type', 'hidden');
-      firstUrlField.setAttribute('name', 'first_url');
-      firstUrlField.setAttribute('value', localStorage.getItem('first-url'));
-      document.getElementById('contact_form').appendChild(firstUrlField);
-      localStorage.removeItem('first-url');
-    }
+function handleSubmit(e) {
+  e.preventDefault();
+  const formData = new FormData(this);
 
-    fetch('https://anadea.info/webhooks/blog_contacts/some_cool_token', {
-      body: formData,
-      method: 'POST',
-    }).then((response) => {
-      console.log(response);
+  fetch('https://anadea.info/webhooks/blog_contacts/some_cool_token', {
+    body: formData,
+    method: 'POST',
+  }).then((response) => {
+    console.log(response);
+  });
+}
+
+function processContactForm() {
+  const contactForm = document.getElementById('contact_form');
+
+  addGuidField(contactForm);
+  addFileNameChangeListener(contactForm);
+  initGtmListener(contactForm);
+  addReferrerUrlField(contactForm);
+  addFirstUrlField(contactForm);
+
+  // add remote IP field
+  fetch('https://api.ipify.org?format=json')
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      createField('remote_ip', json.ip, contactForm);
+    })
+    .catch(function (err) {
+      console.error('Error getting IP Address: '.concat(err));
     });
-  }
 
   contactForm.addEventListener('submit', handleSubmit);
 }
