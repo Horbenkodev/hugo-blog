66cfde9bd6f3383428ac0cc92ce54b4c15bb0f35
his <his@anadeainc.com>
2024-09-30 16:59:10 +0300

    add sub menu & mobile menu

--- themes/anadea/assets/js/_menu.js
+++ themes/anadea/assets/js/_menu.js
@@ -2,7 +2,7 @@ function burgerToggler() {
   let isActive = false;
 
   const burger = document.querySelector('.burger');
-  const menu = document.querySelector('.header__menu');
+  const menu = document.querySelector('.header__mobileMenu');
   const shadow = document.querySelector('.header__shadow');
 
   function activate() {
@@ -32,7 +32,61 @@ function burgerToggler() {
   }
 
   burger.addEventListener('click', handleClick);
+  shadow.addEventListener('click', handleClick);
   window.addEventListener('resize', deactivate);
 }
 
+function menuHover() {
+  function hideAllHoverBlocks() {
+    document.querySelectorAll('.hover__block__container').forEach((block) => {
+      block.style.display = 'none';
+    });
+  }
+
+  document.querySelectorAll('.menu__item').forEach((item) => {
+    item.addEventListener('mouseenter', (event) => {
+      hideAllHoverBlocks();
+
+      const blockType = event.currentTarget.getAttribute('data-hover-block');
+      const targetBlock = document.getElementById(blockType);
+
+      if (targetBlock) {
+        targetBlock.style.display = 'block';
+      }
+    });
+  });
+
+  document.querySelectorAll('.hover__block__container').forEach((container) => {
+    container.addEventListener('mouseleave', hideAllHoverBlocks);
+  });
+}
+
+function mobileMenuToggle() {
+  const menuItems = document.querySelectorAll('.mobileMenu__item');
+
+  menuItems.forEach((item) => {
+    item.addEventListener('click', (event) => {
+      const currentItem = event.currentTarget;
+      const subMenu = currentItem.nextElementSibling;
+
+      const isActive = currentItem.classList.contains('mobileMenu__item--isActive');
+
+      document.querySelectorAll('.mobileMenu__subMenu').forEach((menu) => {
+        menu.classList.remove('mobileMenu__subMenu--isActive');
+      });
+
+      menuItems.forEach((button) => button.classList.remove('mobileMenu__item--isActive'));
+
+      if (!isActive) {
+        if (subMenu && subMenu.classList.contains('mobileMenu__subMenu')) {
+          subMenu.classList.add('mobileMenu__subMenu--isActive');
+          currentItem.classList.add('mobileMenu__item--isActive');
+        }
+      }
+    });
+  });
+}
+
+document.addEventListener('DOMContentLoaded', mobileMenuToggle);
+document.addEventListener('DOMContentLoaded', menuHover, { once: true });
 document.addEventListener('DOMContentLoaded', burgerToggler, { once: true });
